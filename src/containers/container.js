61cdfeba269c1bfb06620fcfcdda5d1f1795ddfa
Ashley Yang <Ashley_Yang@trend.com.tw>
2018-02-23 18:33:13 +0800

    First formal commit for gh-pages branch.

--- src/containers/container.js
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-
-const Container = () => {
-    return (<section>
-        Container
-    </section>);
-};
-
-export default Container;
--- src/containers/footer.js
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-
-const Footer = () => {
-    return (<footer id="contact">
-        Footer
-    </footer>);
-};
-
-export default Footer;
